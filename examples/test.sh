daa8d8730d5c051a9f02c688f01aa6aef4324d63
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2020-08-06 17:34:31 +0200

    Adapt example script to API changes.

--- examples/test.sh
+++ examples/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euf -o pipefail
 
-python -m wgadmin new-network --force wg.json
-python -m wgadmin add-peer wg.json server --endpoint example.org
-python -m wgadmin add-peer wg.json laptop
-python -m wgadmin add-connection wg.json laptop server
-python -m wgadmin list-peers wg.json
+python -m wgadmin new-network --force
+python -m wgadmin add-peer server --endpoint example.org
+python -m wgadmin add-peer laptop
+python -m wgadmin add-connection laptop server
+python -m wgadmin list-peers
