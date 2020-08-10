dc7d142f71ed0c68deac3349fe529fc8aaa6aad7
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2020-08-10 12:34:00 +0200

    Add license and some metadata.

--- wgadmin/main.py
+++ wgadmin/main.py
@@ -1,4 +1,19 @@
 # PYTHON_ARGCOMPLETE_OK
+# Copyright (C) 2020  Fabian Köhler <fabian.koehler@protonmail.ch>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
 import argcomplete
 
@@ -13,7 +28,7 @@ from wgadmin.subcommands import (
 
 
 parser = argparse.ArgumentParser(
-    description="Create, manage and deploy a WireGuard VPN", allow_abbrev=False,
+    description="Create and manage WireGuard VPNs", allow_abbrev=False,
 )
 subparsers = parser.add_subparsers(description="subcommand to run", required=True)
 
